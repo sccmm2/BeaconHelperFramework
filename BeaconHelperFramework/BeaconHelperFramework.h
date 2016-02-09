34e143c16214775ac894619b9c7bb2fc6a844dc5
sung <sccmm2@naver.com>
2016-02-09 20:24:34 +0900

    Initial Commit

---
+++ BeaconHelperFramework/BeaconHelperFramework.h
@@ -0,0 +1,19 @@
+//
+//  BeaconHelperFramework.h
+//  BeaconHelperFramework
+//
+//  Created by Sung on 2016. 2. 9..
+//  Copyright © 2016년 Sung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeaconHelperFramework.
+FOUNDATION_EXPORT double BeaconHelperFrameworkVersionNumber;
+
+//! Project version string for BeaconHelperFramework.
+FOUNDATION_EXPORT const unsigned char BeaconHelperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeaconHelperFramework/PublicHeader.h>
+
+
