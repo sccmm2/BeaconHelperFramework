7f08be014970ad34ee52522166caeeac68a09e72
sung <sccmm2@naver.com>
2016-02-09 20:57:00 +0900

    Initial Commit

--- BeaconHelperFramework/BeaconHelperFramework.h
+++ BeaconHelperFramework/BeaconHelperFramework.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char BeaconHelperFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <BeaconHelperFramework/PublicHeader.h>
 
-
+#import <BeaconHelperFramework/BearconManager.h>
